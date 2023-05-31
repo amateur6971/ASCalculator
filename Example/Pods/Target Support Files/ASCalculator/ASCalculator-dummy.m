924f57671a79c22f569092e3fa75490a48afe16d
Neosoft <neosoft@apples-MacBook-Pro.local>
2023-05-31 14:27:41 +0530

    first commit

---
+++ Example/Pods/Target Support Files/ASCalculator/ASCalculator-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ASCalculator : NSObject
+@end
+@implementation PodsDummy_ASCalculator
+@end
---
+++ Example/Pods/Target Support Files/Pods-ASCalculator_Example/Pods-ASCalculator_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ASCalculator_Example : NSObject
+@end
+@implementation PodsDummy_Pods_ASCalculator_Example
+@end
